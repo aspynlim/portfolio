9cc167b5bf523024c9459b8db803ccef490b89ca
JenLim <cmdlhz@gmail.com>
2021-06-26 23:54:06 +0900

    [2] Blog : reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './scss/styles.scss'
-import image from './images/react.png'
-import bird from './images/bird.svg'
-import { ClickCounter } from './components/ClickCounter'
 
-export const App = () => {
+function App() {
   return (
-    <>
-      <h1>BYE! 123 Just do it!</h1>
-      <h1>{process.env.NODE_ENV}</h1>
-      <h1>{process.env.name}</h1>
-      <img src={image} alt="React Logo" width="200" />
-      <img src={bird} alt="Bird" width="200" />
-      <ClickCounter />
-    </>
+    <div>
+      <h1>Hyejung(Aspyn) Lim</h1>
+    </div>
   )
 }
+
+export default App
