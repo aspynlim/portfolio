903099b16849960180381c6ef4778f9720ece6f5
aspynlim <aspyn.lim@gmail.com>
2021-07-02 16:24:23 +0900

    [2] Blog : Navigating Programmatically + Getting Started with Fetching Data

--- src/pages/projects/blogProject/AllBlogPosts.tsx
+++ src/pages/projects/blogProject/AllBlogPosts.tsx
@@ -1,37 +1,32 @@
+import { useState } from 'react'
 import BlogPostList from '../../../components/blogPosts/BlogPostList'
-import { BlogPost } from '../../../components/Interfaces/BlogPost'
-
-const DUMMY_DATA: BlogPost[] = [
-  {
-    id: 'b1',
-    title: 'JavaScript ES6 Tutorials',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    description:
-      'Topics : Let, Default Parameters, Spread Operator, Template Strings, Object Literal Enhancements, New String Methods, Arrow Functions, Sets, Generators',
-  },
-  {
-    id: 'b2',
-    title: 'Asynchronous JavaScript',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    description:
-      'Topics : Ajax Request, Callback Functions, Promises, Generators',
-  },
-  {
-    id: 'b3',
-    title: 'Object Oriented JavaScript',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    description:
-      'Topics : Object Literals, Class Methods, Method Chaining, Class Inheritance, Constructors (under the hood), Prototype, Inheritance',
-  },
-]
 
 function AllBlogPosts() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [loadedBlogPosts, setLoadedBlogPosts] = useState([])
+
+  fetch(
+    'https://react-blog-26569-default-rtdb.asia-southeast1.firebasedatabase.app/blogPosts.json'
+  )
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      setIsLoading(false)
+      setLoadedBlogPosts(data)
+    })
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    )
+  }
+
   return (
     <div>
-      <BlogPostList items={DUMMY_DATA} />
+      <BlogPostList items={loadedBlogPosts} />
     </div>
   )
 }
