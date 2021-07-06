543f1350e78921d2aa755a0a71f80c6de1318a7c
aspynlim <aspyn.lim@gmail.com>
2021-07-06 17:58:31 +0900

    [2] Blog : Removed 'Favorites' - solved issues

--- src/pages/projects/BlogProjectMain.tsx
+++ src/pages/projects/BlogProjectMain.tsx
@@ -3,31 +3,31 @@ import { Route, Switch } from 'react-router-dom'
 import BlogProject from './blogProject/BlogProject'
 import AllBlogPosts from './blogProject/AllBlogPosts'
 import NewBlogPost from './blogProject/NewBlogPost'
-import FavoriteBlogPosts from './blogProject/FavoriteBlogPosts'
+// import FavoriteBlogPosts from './blogProject/FavoriteBlogPosts'
 
 import BlogProjectNav from '../../components/layout/BlogProjectNav'
-import { FavoritesContextProvider } from '../../store/favorites-context'
+// import { FavoritesContextProvider } from '../../store/favorites-context'
 
 function BlogProjectMain() {
   return (
     <div>
-      <FavoritesContextProvider>
-        <BlogProjectNav />
-        <Switch>
-          <Route path="/projects/blog" exact>
-            <BlogProject />
-          </Route>
-          <Route path="/projects/blog/all">
-            <AllBlogPosts />
-          </Route>
-          <Route path="/projects/blog/new">
-            <NewBlogPost />
-          </Route>
-          <Route path="/projects/blog/favorites">
+      {/* <FavoritesContextProvider> */}
+      <BlogProjectNav />
+      <Switch>
+        <Route path="/projects/blog" exact>
+          <BlogProject />
+        </Route>
+        <Route path="/projects/blog/all">
+          <AllBlogPosts />
+        </Route>
+        <Route path="/projects/blog/new">
+          <NewBlogPost />
+        </Route>
+        {/* <Route path="/projects/blog/favorites">
             <FavoriteBlogPosts />
-          </Route>
-        </Switch>
-      </FavoritesContextProvider>
+          </Route> */}
+      </Switch>
+      {/* </FavoritesContextProvider> */}
     </div>
   )
 }
